35807cca726e98adb19e970abeacb568e6fde6a0
Félix Luis López <fl.lopez.321@gmail.com>
2026-06-19 16:00:11 -0500

    Reestructurando

--- src/app/components/metricascomponent/metricas-insert/metricas-insert.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-metricas-insert',
-  imports: [],
-  templateUrl: './metricas-insert.html',
-  styleUrl: './metricas-insert.css',
-})
-export class MetricasInsert {}
--- src/app/components/metricascomponent/metricas-list/metricas-list.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-metricas-list',
-  imports: [],
-  templateUrl: './metricas-list.html',
-  styleUrl: './metricas-list.css',
-})
-export class MetricasList {}
