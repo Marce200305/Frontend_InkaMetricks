29daf9d038b9512572940db9f4840e6ee1e21897
Félix Luis López <fl.lopez.321@gmail.com>
2026-06-22 21:48:55 -0500

    solución a error de environment con servicios de apis

--- src/app/services/youtube-service.ts
+++ src/app/services/youtube-service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
-const BASE = 'http://localhost:8080/youtube';
+const BASE = `${environment.base}/youtube`;
 
 @Injectable({ providedIn: 'root' })
 export class YoutubeService {
